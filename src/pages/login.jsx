1e56b84874290d49973ffd7847ba070413e22bea
Alichey <idenchrstn9@gmail.com>
2026-05-21 20:47:14 +0700

    update kesekian kalinya

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -12,8 +12,34 @@ import {
     Text,
 } from "@chakra-ui/react";
 
-import logo from "../assets/hero.png";
+import logo from "../assets/icons/logo.png";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleLogin = async () => {
+        const url = "https://localhost/inventarisweb/proseslogin.php";
+        const body = { username: username, password: password };
+        
+        try{
+            const response = await axios.post(url, body);
+            if (response.data.STATUS == "BERHASIL") {
+                localStorage.setItem("usenameLS", response.data.DATA[0]["username"]);
+                localStorage.setItem("namaLS", response.data.DATA[0]["nama"]);
+                navigate("/Dashboard");
+                console.log("berhasil");
+            } else {
+                navigate("/");
+                console.log("gagal");
+            }
+        } catch (error) {
+            console.log(error);
+        } 
+    };
     return (
         <>
             <Box 
@@ -34,9 +60,28 @@ const Login = () => {
                             <Text textAlign = "center">My Inventory</Text>
                         </CardTitle>
                         <CardBody gapY = "10px">
-                            <Input placeholder = "Username" type = "text"/>
-                            <Input placeholder = "Password" type = "password"/>
-                            <Button backgroundColor="teal" color="white" borderRadius = "10px">
+                            <Input 
+                                placeholder = "Username" 
+                                type = "text" 
+                                onChange = {(e) => {
+                                setUsername(e.target.value);
+                            }}
+                            />
+                            <Input 
+                                placeholder = "Password" 
+                                type = "password"
+                                onChange = {(e) => {
+                                setPassword(e.target.value);
+                            }}
+                            />
+                            <Button 
+                                backgroundColor="teal" 
+                                color="white" 
+                                borderRadius = "10px"
+                                onClick={() => {
+                                    handleLogin();
+                                }}
+                            >
                                 <Text>LOGIN</Text>
                             </Button>
                         </CardBody>
